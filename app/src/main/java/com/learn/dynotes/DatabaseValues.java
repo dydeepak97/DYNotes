13c77a0e3b45497e336880c8f75d95ebd054159f
dydeepak97 <dydeepak97@gmail.com>
2017-02-25 20:35:31 +0530

    Adding Classes

--- app/src/main/java/com/learn/dynotes/DatabaseValues.java
+++ app/src/main/java/com/learn/dynotes/DatabaseValues.java
@@ -3,4 +3,27 @@ package com.learn.dynotes;
 
 
 public class DatabaseValues {
+
+
+    public  static final int DATABSE_VERSION = 1;
+
+    public static final String DATABSE_NAME = "dynotes";
+
+    public static final String TABLE_NOTES= "notes";
+
+    //Notes table column names
+    public static final String NOTES_ID="id";
+    public static final String NOTES_TITLE="title";
+    public static final String NOTES_DESCRIPTION="description";
+
+
+    //Create Table Queries
+    public static final String TABLE_NOTES_CREATE="CREATE TABLE IF NOT EXISTS" + TABLE_NOTES +
+            "(" + NOTES_ID +"INTEGER PRIMARY KEY,"+ NOTES_TITLE +"TEXT," + NOTES_DESCRIPTION + "TEXT)";
+
+    //Drop Table Queries
+    public static final String TABLE_NOTES_DROP="DROP TABLE IF EXITS" + TABLE_NOTES;
+
+
+
 }
