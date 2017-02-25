c749fb88e28935ea0d2c22b9685bdf7c48f58bce
dydeepak97 <dydeepak97@gmail.com>
2017-02-25 20:01:29 +0530

    Initial commit

---
+++ app/src/main/java/com/learn/dynotes/Note.java
@@ -0,0 +1,31 @@
+package com.learn.dynotes;
+
+
+public class Note {
+
+    private int id;
+    private String title;
+    private String description;
+
+    public void Note(){
+
+    }
+
+    public int getId(){ return this.id; }
+
+    public  void  setId(int id) {   this.id=id;  }
+
+    public String getTitle(){return title;}
+
+    public  void  setTitle(String title){ this.title=title;}
+
+    public  String getDescription(){ return description;}
+
+    public void setDescription(String description){ this.description=description;}
+
+
+
+
+}
+
+
