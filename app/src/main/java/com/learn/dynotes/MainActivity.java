7790e3a471de11cd82208c5610760ac2a77f02e7
dydeepak97 <dydeepak97@gmail.com>
2017-03-15 21:57:27 +0530

    Layout Finalized

--- app/src/main/java/com/learn/dynotes/MainActivity.java
+++ app/src/main/java/com/learn/dynotes/MainActivity.java
@@ -1,12 +1,32 @@
 package com.learn.dynotes;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
+
+    Button addNote;
+    DatabaseHandler databaseHandler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        addNote = (Button) findViewById(R.id.addNote);
+
+
+        databaseHandler= new DatabaseHandler(this);
+
+
+
+    }
+
+
+    public void onClick(View v){
+        Intent myIntent= new Intent(MainActivity.this,EditNoteActivity.class);
+
     }
 }
