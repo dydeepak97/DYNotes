4e1673f175b3b1f1c2fbf82abb2deb83fe43024f
dydeepak97 <dydeepak97@example.com>
2018-08-24 18:22:56 +0530

    Login Button Removed

--- app/src/main/java/com/dydeepak97/dynotes/loginActivity.java
+++ app/src/main/java/com/dydeepak97/dynotes/loginActivity.java
@@ -4,17 +4,17 @@ import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class loginActivity extends Activity {
 
     public static final String PREF_NAME="LoginDetails";
-
-
-
 
 
     static int attempts=3;
@@ -28,7 +28,18 @@ public class loginActivity extends Activity {
         setContentView(R.layout.activity_login);
         decide();
 
+        passBox=(EditText) this.findViewById(R.id.passBox);
+        passBox.setOnEditorActionListener(new EditText.OnEditorActionListener(){
+            @Override
+            public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) {
+                if (i == EditorInfo.IME_ACTION_DONE){
+                    login(log);
+                    return true;
+                }
+                return false;
+            }
 
+        } );
 
     }
 
@@ -39,15 +50,18 @@ public class loginActivity extends Activity {
             Intent mainIntent=new Intent(loginActivity.this,MainActivity.class);
             startActivity(mainIntent);
 
+
         }
     }
+
+
 
     public void login(View v){
 
 
 
 
-        passBox=(EditText) this.findViewById(R.id.passBox);
+
         password=passBox.getText().toString();
         SharedPreferences logindata= getSharedPreferences(loginActivity.PREF_NAME, 0);
 
